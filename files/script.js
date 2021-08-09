8dff1cb7fee97025ccc16ebb898bc8a7ab005c6a
Henrique Garcia <henrique.garcia@moovin.com.br>
2021-08-09 02:33:15 -0300

    Ajustes iniciais na funçao de contador

--- files/script.js
+++ files/script.js
@@ -7,6 +7,7 @@ function SliderController() {
   const carouselContainer = document.querySelector('.dc-carousel')
   const carouselView = document.querySelector('.dc-carousel-slides')
   const carouselSliders = document.querySelectorAll('.dc-carousel-slide')
+  const carouselCounter = document.querySelector('.dc-carousel-counters')
 
   const arrowElements = document.querySelectorAll('.default__arrow')
 
@@ -20,15 +21,16 @@ function SliderController() {
 
   let slidesCounter = 0
   let currentWidth = 0
+  let index = 1
 
   this.start = async () => {
-    await this.renderConfigs(configs)
     await this.getSlides()
+    await this.renderConfigs(configs)
   }
 
-  this.renderConfigs = (general) => {
-    if (general.loop) {
-      if (general.autoplay) {
+  this.renderConfigs = (configs) => {
+    if (configs.loop) {
+      if (configs.autoplay) {
         this.sliderControl(true, true)
       } else {
         this.sliderControl(true, false)
@@ -37,7 +39,18 @@ function SliderController() {
       this.sliderControl(false)
     }
 
-    carouselView.style.transition = general.transition_time ? `all ${general.transition_time}s` : "all 0.5s"
+    if (configs.counter) {
+      // const counterElement = document.createElement('div')
+      // counterElement.classList.add('dc-carousel-counter')
+
+      // for (var i = 0; i < 10; i++) {
+      //   console.log(i)
+      //   carouselCounter.appendChild(counterElement)
+      // }
+      this.counterControl()
+    }
+
+    carouselView.style.transition = configs.transition_time ? `all ${configs.transition_time}s` : "all 0.5s"
   }
 
   this.getSlides = () => {
@@ -48,7 +61,6 @@ function SliderController() {
 
   this.sliderControl = (loop, autoplay) => {
     const widthCarousel = carouselContainer.offsetWidth
-    let index = 1
 
     carouselView.style.left = `${currentWidth}px`
 
@@ -60,22 +72,26 @@ function SliderController() {
               index = slidesCounter
               currentWidth = (slidesCounter - 1) * -widthCarousel
               carouselView.style.left = `${currentWidth}px`
+              this.counterControl(index)
               return
             }
             index -= 1
             currentWidth += widthCarousel
             carouselView.style.left = `${currentWidth}px`
+            this.counterControl(index)
             return
           } else {
             if (index + 1 > slidesCounter) {
               index = 1
               currentWidth = 0
               carouselView.style.left = `${currentWidth}px`
+              this.counterControl(index)
               return
             }
             index += 1
             currentWidth -= widthCarousel
             carouselView.style.left = `${currentWidth}px`
+            this.counterControl(index)
             return
           }
         }))
@@ -86,11 +102,13 @@ function SliderController() {
               index = 1
               currentWidth = 0
               carouselView.style.left = `${currentWidth}px`
+              this.counterControl(index)
               return
             }
             index += 1
             currentWidth -= widthCarousel
             carouselView.style.left = `${currentWidth}px`
+            this.counterControl(index)
           }, configs.autoplay_time * 1000 + configs.transition_time * 1000);
         }
         break
@@ -122,5 +140,27 @@ function SliderController() {
         // }
         break
     }
+  }
+
+  this.counterControl = (idx) => {
+    const carouselCounters = document.querySelectorAll('.dc-carousel-counter')
+
+    if (idx) {
+      Array.from(carouselCounters).forEach((ele) => {
+        ele.classList.remove('selected')
+        if (ele.getAttribute('index') == idx) {
+          ele.classList.add('selected')
+        }
+      })
+      return
+    }
+
+    Array.from(carouselCounters).forEach((ele, i) => {
+      ele.setAttribute('index', i+1)
+
+      ele.addEventListener('click', (ele) => {
+        console.log(ele.path[0].getAttribute('index'))
+      })
+    })
   }
 }
